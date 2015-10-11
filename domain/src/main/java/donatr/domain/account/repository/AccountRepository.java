4cfad929e01a1a762894b558d4adb9f6650ef1da
vileda <vileda@vileda.cc>
2015-10-11 20:38:55 +0200

    no need for snapshots

--- domain/src/main/java/donatr/domain/account/repository/AccountRepository.java
+++ domain/src/main/java/donatr/domain/account/repository/AccountRepository.java
@@ -6,7 +6,6 @@ import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.jpa.SimpleEntityManagerProvider;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventsourcing.HybridJpaRepository;
-import org.axonframework.eventsourcing.SnapshotterTrigger;
 import org.axonframework.eventstore.EventStore;
 import org.axonframework.unitofwork.CurrentUnitOfWork;
 import org.axonframework.unitofwork.DefaultUnitOfWork;
@@ -18,12 +17,11 @@ import javax.persistence.EntityManager;
 public class AccountRepository {
 	private HybridJpaRepository<Account> eventSourcingRepository;
 
-	public AccountRepository(EventStore eventStore, EventBus eventBus, SnapshotterTrigger snapshotterTrigger, EntityManager entityManager) {
+	public AccountRepository(EventStore eventStore, EventBus eventBus, EntityManager entityManager) {
 		EntityManagerProvider entityManagerProvider = new SimpleEntityManagerProvider(entityManager);
 		eventSourcingRepository = new HybridJpaRepository<>(entityManagerProvider, Account.class);
 		eventSourcingRepository.setEventBus(eventBus);
 		eventSourcingRepository.setEventStore(eventStore);
-		//eventSourcingRepository.setSnapshotterTrigger(snapshotterTrigger);
 	}
 
 	public Account load(Object aggregateIdentifier) {
--- domain/src/main/java/donatr/domain/account/repository/AccountRepositoryProducer.java
+++ domain/src/main/java/donatr/domain/account/repository/AccountRepositoryProducer.java
@@ -1,21 +1,13 @@
 package donatr.domain.account.repository;
 
-import donatr.domain.account.Account;
 import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventsourcing.*;
 import org.axonframework.eventstore.EventStore;
-import org.axonframework.eventstore.SnapshotEventStore;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.UserTransaction;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Singleton
@@ -29,25 +21,10 @@ public class AccountRepositoryProducer {
 	@Inject
 	EventBus eventBus;
 
-	private UserTransaction getUserTransaction() throws NamingException {
-		return ((UserTransaction) new InitialContext().lookup("java:jboss/UserTransaction"));
-	}
 
 	@Produces @Singleton
 	public AccountRepository getEventRepository() {
 		System.out.println("init repository");
-		return new AccountRepository(eventStore, eventBus, getSnapshotterTrigger(), entityManager);
-	}
-
-	public SnapshotterTrigger getSnapshotterTrigger() {
-		EventCountSnapshotterTrigger snapshotterTrigger = new EventCountSnapshotterTrigger();
-		snapshotterTrigger.setTrigger(1);
-		AggregateSnapshotter snapshotter = new AggregateSnapshotter();
-		List<AggregateFactory<?>> aggregateFactories = new ArrayList<>();
-		aggregateFactories.add(new GenericAggregateFactory<>(Account.class));
-		snapshotter.setAggregateFactories(aggregateFactories);
-		snapshotter.setEventStore((SnapshotEventStore) eventStore);
-		snapshotterTrigger.setSnapshotter(snapshotter);
-		return snapshotterTrigger;
+		return new AccountRepository(eventStore, eventBus, entityManager);
 	}
 }
